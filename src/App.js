2e23935ae8102d2ee2720410c0b4db3e357c2e45
DiviS <35492002+Divi20@users.noreply.github.com>
2020-09-29 17:07:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import MainComponent from "./components/MainComponent";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
+}
